2059a7dfdc113add029dae5f5955dd1cba42f563
nikolay-dimitrov256 <raystlin7@gmail.com>
2026-09-08 16:25:56 +0300

    Fixed the add button behaviour when bottom menu is expanded

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,14 @@ window.addEventListener('DOMContentLoaded', initPage);
 
 function initPage() {
     initActionsToggles();
+
+    initMenuToggle();
 }
 
 function initActionsToggles() {
     const bodyElement = document.querySelector('body');
 
-    bodyElement.addEventListener('click', (event) => {
+    document.body.addEventListener('click', (event) => {
         const actionsToggleAElement = event.target.closest('.actions-toggle');
 
         if (!actionsToggleAElement) {
@@ -21,5 +23,24 @@ function initActionsToggles() {
         const actionsDivElement = actionsToggleAElement.nextElementSibling;
         
         actionsDivElement.classList.toggle('visible');
+    });
+}
+
+function initMenuToggle() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const addButton = document.querySelector('.add-button');
+    const menuToggleLabel = document.querySelector('label[for=menu-toggle]');
+
+    menuToggle.addEventListener('change', () => {
+        addButton.classList.toggle('menu-toggle-checked', menuToggle.checked);
+    });
+
+    document.body.addEventListener('click', (event) => {
+        const menuToggleIsClicked = event.target === menuToggle || menuToggleLabel.contains(event.target);
+        
+        if (!menuToggleIsClicked) {
+            menuToggle.checked = false;
+            addButton.classList.remove('menu-toggle-checked');
+        }
     });
 }
